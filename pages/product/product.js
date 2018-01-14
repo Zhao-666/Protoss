3e6fc344cc9a70f552a74c722c18eccff63e749e
Zhao-666 <2268699370@qq.com>
2018-01-14 23:35:32 +0800

    11-18 增加了product组件，用于展示单个商品的详情 完成了详情的head_img展示，使用picker组件增加了商品商量选择功能

--- pages/product/product.js
+++ pages/product/product.js
@@ -1,18 +1,40 @@
 // pages/product/product.js
+
+import { Product } from './product-model.js'
+var product = new Product()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    productCount: 1,
+    countsArray: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options)
     var id = options.id
+    this.setData({
+      id
+    })
+    this._loadData()
   },
+  _loadData: function () {
+    product.getDetailInfo(this.data.id, (data) => {
+      this.setData({
+        product: data
+      })
+    })
+  },
+  bindPickerChange: function (event) {
+    var index = event.detail.value
+    var selectedCount = this.data.countsArray[index]
+    this.setData({
+      productCount: selectedCount
+    })
+  }
 })
