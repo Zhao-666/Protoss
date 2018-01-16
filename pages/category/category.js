5c63aae82d0a482235b699e57341adcc4dbf9b02
Zhao-666 <2268699370@qq.com>
2018-01-16 22:14:43 +0800

    11-23 完成了商品分类页面。使用了热加载的方式来加载对应分类的商品，可以降低服务器的负担

--- pages/category/category.js
+++ pages/category/category.js
@@ -1,66 +1,75 @@
 // pages/category/category.js
+import { Category } from './category-model.js'
+var category = new Category()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    transClassArr: ['tanslate0', 'tanslate1', 'tanslate2', 'tanslate3', 'tanslate4', 'tanslate5'],
+    selectedCategory: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    this._loadData(options)
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+  _loadData: function (options) {
+    category.getCategoryType((categoryData) => {
+      this.setData({
+        categoryTypeArr: categoryData
+      })
+      category.getProductsByCategory(categoryData[0].id, (data) => {
+        var dataObj = {
+          products: data,
+          topImgUrl: categoryData[0].img.url,
+          title: categoryData[0].name
+        }
+        this.setData({
+          categoryInfo0: dataObj
+        })
+      })
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
+  changeCategory: function (event) {
+    var index = category.getDataSet(event, 'index')
+    var id = category.getDataSet(event, 'id')
+    this.setData({
+      selectedCategory: index
+    })
+    //如果数据是第一次请求
+    if (!this.isLoadedData(index)) {
+      category.getProductsByCategory(id, (data) => {
+        this.setData(this.getDataObjForBind(index, data));
+      });
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
+  isLoadedData: function (index) {
+    if (this.data['categoryInfo' + index]) {
+      return true
+    }
+    return false
   },
+  getDataObjForBind: function (index, data) {
+    var obj = {},
+      arr = [0, 1, 2, 3, 4, 5],
+      baseData = this.data.categoryTypeArr[index];
+    for (var item in arr) {
+      if (item == arr[index]) {
+        obj['categoryInfo' + item] = {
+          products: data,
+          topImgUrl: baseData.img.url,
+          title: baseData.name
+        };
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+        return obj;
+      }
+    }
   }
+
 })
